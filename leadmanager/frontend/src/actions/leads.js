db5b4204efb2e73f5d6e66ac207b1a5b58055460
PlanetfunkDevOps <levart.kev@gmail.com>
2019-04-10 12:47:23 -0500

    completed token validation to add and delete leads once loggedin/registered

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import { createMessage, returnErrors } from './messsages';
+import { tokenConfig } from './auth'
 
 import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from './types';
 
 /* GET LEADS */
-export const getLeads = () => dispatch => {
+export const getLeads = () => (dispatch, getState) => {
     axios
-        .get('/api/leads/')
+        .get('/api/leads/', tokenConfig(getState))
         .then(res => {
             dispatch({
                 type: GET_LEADS,
@@ -16,9 +17,9 @@ export const getLeads = () => dispatch => {
 };
 
 /* ADD LEAD */
-export const addLead = (lead) => dispatch => {
+export const addLead = (lead) => (dispatch, getState) => {
     axios
-        .post('/api/leads/', lead)
+        .post('/api/leads/', lead, tokenConfig(getState))
         .then(res => {
             dispatch(createMessage({
                 addLead: 'Lead Added'
@@ -31,9 +32,9 @@ export const addLead = (lead) => dispatch => {
 };
 
 /* DELETE LEAD */
-export const deleteLead = id => dispatch => {
+export const deleteLead = id => (dispatch, getState) => {
     axios
-        .delete(`/api/leads/${id}`)
+        .delete(`/api/leads/${id}`, tokenConfig(getState))
         .then(res => {
             dispatch(createMessage({
                 deleteLead: 'Lead Deleted'
