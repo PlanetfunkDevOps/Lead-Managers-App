965571457690cf923d39b9f512e7ba2d02a7ee7e
PlanetfunkDevOps <levart.kev@gmail.com>
2019-04-10 11:59:46 -0500

    completed login/logout functionality

--- leadmanager/frontend/src/actions/auth.js
+++ leadmanager/frontend/src/actions/auth.js
@@ -1,29 +1,14 @@
 import axios from 'axios';
 import { returnErrors } from './messsages';
 
-import { USER_LOADED, USER_LOADING, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL } from './types';
+import { USER_LOADED, USER_LOADING, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT_SUCCESS } from './types';
 
 /* CHECK TOKEN && LOAD USER */
 export const loadUser = () => (dispatch, getState) => {
     /* User Loading */
     dispatch({ type: USER_LOADING });
 
-    /* Get token from state */
-    const token = getState().auth.token;
-
-    /* Headers */
-    const config = {
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }
-
-    /* If token add to headers config */
-    if (token) {
-        config.headers['Authorization'] = `Token ${token}`;
-    }
-
-    axios.get('/api/auth/user', config)
+    axios.get('/api/auth/user', tokenConfig(getState))
         .then(res => {
             dispatch({
                 type: USER_LOADED,
@@ -64,3 +49,36 @@ export const login = (username, password) => dispatch => {
             });
         });
 };
+
+/* Logout User */
+export const logout = () => (dispatch, getState) => {
+    axios.post('/api/auth/logout/', null, tokenConfig(getState))
+        .then(res => {
+            dispatch({
+                type: LOGOUT_SUCCESS
+            });
+        }).catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status));
+        });
+};
+
+
+/* Setup config with token - helper function */
+export const tokenConfig = getState => {
+    /* Get token from state */
+    const token = getState().auth.token;
+
+    /* Headers */
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    /* If token add to headers config */
+    if (token) {
+        config.headers['Authorization'] = `Token ${token}`;
+    }
+
+    return config
+}
